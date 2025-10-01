5c775c7942d1d420cf3436c887fc5db5cbb2a131
PNS <solovjov_pn@mail.ru>
2025-10-01 10:49:46 +0300

    adopt adopt MessageRepo to save all message  data + something more as template to develop& test

--- src/main/java/pn/back/repo/MessageRepositoryImpl.java
+++ src/main/java/pn/back/repo/MessageRepositoryImpl.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import pn.back.entities.Comment;
 import pn.back.entities.Message;
+import pn.back.mappers.CommentMapper;
 import pn.back.mappers.MessageMapper;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +25,8 @@ public class MessageRepositoryImpl implements MessageRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private CommentMapper commentMapper;
     @Autowired
     private MessageMapper messageMapper;
 
@@ -111,19 +116,41 @@ public class MessageRepositoryImpl implements MessageRepository {
 
     @Override
     public Optional<Message> updateTitle(long id, String title) {
+
+        String sql = "    UPDATE pract.blog.messages  " +
+                "             SET  " +
+                "                  title = '" + title + "'" +
+                "     WHERE id =  " + id;
+        log.info(
+                "\n SQL UPDATE RUN \n{}", sql
+        );
         try {
-            String sql = "    UPDATE pract.blog.messages  " +
-                    "             SET  " +
-                    "                  title = '" + title + "'" +
-                    "     WHERE id =  " + id;
-            log.info(
-                    "\n SQL UPDATE RUN \n{}", sql
-            );
             int numberOfUpdates = jdbcTemplate.update(sql);
             if (numberOfUpdates == 0) log.info("No any updates");
             return findById(id);
         } catch (Exception e) {
             log.info("no Message of id {}", id);
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<Message> incrementCommentsCount(Message message) {
+        long commentsCount = message.getCommentsCount() + 1;
+        String sql = "    UPDATE pract.blog.messages  " +
+                "             SET  " +
+                "                  comments_count = " + commentsCount +
+                "     WHERE id =  " + message.getId();
+        log.info(
+                "\n SQL UPDATE RUN \n{}", sql
+        );
+        try {
+            int numberOfUpdates = jdbcTemplate.update(sql);
+            if (numberOfUpdates == 0) log.info("No any updates");
+            System.out.println(" NNNEEEWWW  MMESS " + findById(message.getId()).get());
+            return findById(message.getId());
+        } catch (Exception e) {
+            log.info("no Message of found");
             return Optional.empty();
         }
     }
@@ -149,23 +176,28 @@ public class MessageRepositoryImpl implements MessageRepository {
 
     @Override
     public Optional<Message> save(Message message) {
-
+        System.out.println("  \t MESSAGE \n" + message);
+        List<String> tags = Arrays.asList(message.getTags()).stream().toList()
+                .stream().map((t) -> "'" + t + "'").toList();
+        System.out.println("STR-LIST : " + tags);
+        String sql = " " +
+                "INSERT INTO pract.blog.messages " +
+                " (title, content, tags ) " +
+                " VALUES ( '" + message.getTitle() + "'" +
+                ", '" + message.getContent() + "' ," +
+                " ARRAY[" + tags + "] )";
+        System.out.println(
+                "\n SQL UPDATE RUN \n" + sql
+        );
         try {
-            String sql = " " +
-                    "INSERT INTO pract.blog.messages " +
-                    " (title, content) " +
-                    " VALUES ( '" + message.getTitle() + "'" +
-                    ", '" + message.getContent() + "' )";
-            System.out.println(
-                    "\n SQL UPDATE RUN \n" + sql
-            );
+
             int numberOfUpdates = jdbcTemplate.update(sql);
             long lastId = jdbcTemplate.queryForObject(
                     "SELECT MAX(id) FROM  pract.blog.messages", Long.class
             );
             System.out.println("\n\n LAST ID " + lastId);
 
-            return findById(lastId);
+            return Optional.of(message);
         } catch (Exception e) {
             log.info("no Message  inputs");
             return Optional.empty();
@@ -205,6 +237,18 @@ public class MessageRepositoryImpl implements MessageRepository {
         } catch (Exception e) {
             log.info("no Message of id {}", id);
             return false;
+        }
+    }
+
+    @Override
+    public List<Comment> commentCount(Message message) {
+        String sql =
+                "SELECT COUNT(*) FROM pract.blog.comments   WHERE message_key = " + message.getId() + " ORDER BY id ASC";
+        System.out.println("SQL ::: " + sql);
+        try {
+            return jdbcTemplate.query(sql, commentMapper);
+        } catch (Exception e) {
+            return List.of();
         }
     }
 
