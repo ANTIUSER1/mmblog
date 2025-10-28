b95350dc445096cf125f673e9f38cecd67f8e1a6
PNS <solovjov_pn@mail.ru>
2025-10-28 22:06:19 +0300

    PropertySource  --------->>>>>>>>TestPropertySource

--- src/test/java/pn/back/config/WebConfigTest.java
+++ src/test/java/pn/back/config/WebConfigTest.java
@@ -1,11 +1,27 @@
 package pn.back.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import pn.back.mappers.CommentMapper;
+import pn.back.mappers.MessageMapper;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"pn.back"})
+@ComponentScan(basePackages = {"pn.back.controllers", "pn.back.services", "pn.back.repo"})
+@Import({MessageMapper.class, CommentMapper.class})
+@TestPropertySource(locations = "classpath:test-application.properties")
 public class WebConfigTest {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Bean
+    String applicationName() {
+        return appName;
+    }
 }
